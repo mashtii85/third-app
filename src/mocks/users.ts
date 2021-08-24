fdd804796b9af015a2abdfb0ce93e749b0d11413
Kristian Heruc <kristian@heruc.com>
2021-08-24 12:59:08 +0200

    feat: refactor logins to use Users mock data

---
+++ src/mocks/users.ts
@@ -0,0 +1,24 @@
+/**
+ * Mocks - Users
+ *
+ */
+
+// Types
+import { User } from '../types/user'
+
+ export const Users: User[] = [
+  {
+    id: 1,
+    account_type: 'admin',
+    email: 'admin@realworldacademies.com',
+    name_first: 'Demo Admin',
+    name_last: 'RWA'
+  },
+  {
+    id: 2,
+    account_type: 'user',
+    email: 'demo@realworldacademies.com',
+    name_first: 'Demo User',
+    name_last: 'RWA'
+  },
+]
--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,5 +1,7 @@
 /**
  * Auth - Login
+ *
+ * Note: uses mock data to login
  */
 
 // Next
@@ -8,33 +10,17 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 // JWT
 import jwt from 'jsonwebtoken'
 
+// Mocks
+import { Users } from '../../../mocks/users'
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    let user = null
+    const user = Users.find(u => u.email === req.body.email)
 
-    // Check for hardcoded credentials
-    if (req.body.email === 'demo@realworldacademies.com' && req.body.password === 'Lms1234!') {
-      user = {
-        id: 1,
-        account_type: 'user',
-        email: 'demo@realworldacademies.com',
-        name_first: 'Demo User',
-        name_last: 'RWA'
-      }
-    } else if (req.body.email === 'admin@realworldacademies.com' && req.body.password === 'Lms1234!') {
-      user = {
-        id: 1,
-        account_type: 'admin',
-        email: 'admin@realworldacademies.com',
-        name_first: 'Demo Admin',
-        name_last: 'RWA'
-    }
-    } else {
-      res.status(401).json({ error: 'Email or password not correct' })
-      return null
+    if (!user || req.body.password !== 'Lms1234!') {
+      return res.status(401).json({ error: 'Email or password not correct' })
     }
 
-    // Generate token
     const token = jwt.sign({ user }, process.env.JWT_PRIVATE_KEY, {
       algorithm: 'RS512',
       expiresIn: '7d'
