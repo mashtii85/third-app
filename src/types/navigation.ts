8cba719d5bacf76892bac83687b81a2aaeeee741
Kristian Heruc <kristian@heruc.com>
2021-08-24 12:07:31 +0200

    feat: add types

---
+++ src/types/navigation.ts
@@ -0,0 +1,15 @@
+/**
+ * Types - Navigation
+ */
+
+export interface NavLink {
+  id: string,
+  name?: string,
+  to?: string,
+  type?: any
+}
+
+export interface Navigation {
+  left?: NavLink[],
+  right?: NavLink[]
+}
---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+/**
+ * Types - User
+ */
+
+export interface User {
+  id: number,
+  type: string,
+  email: string,
+  nameFirst: string,
+  nameLast: string
+}
