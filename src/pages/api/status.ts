a77c38567ec9302669ea4285f2a658aed40091f1
Kristian Heruc <kristian@heruc.com>
2021-09-09 16:25:18 +0200

    feat: add status endpoint to the api

---
+++ src/pages/api/status.ts
@@ -0,0 +1,11 @@
+/**
+ * Status
+ *
+ */
+
+// Next
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({ status: 'success' })
+}
