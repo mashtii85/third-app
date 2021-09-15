d1e0613db9c60d94fb8be488e4f66c06606fb4ee
Kristian Heruc <kristian@heruc.com>
2021-09-15 09:50:59 +0200

    chore: uncomment PieChart

--- src/components/courses/chart.tsx
+++ src/components/courses/chart.tsx
@@ -32,7 +32,7 @@ export const CourseProgressChart = ({ pieData }: CourseProgressChartProps) => {
   return (
     <StyledHeader>
       <StyledPieChart>
-        {/* <PieChart data={data} {...arg} /> */}
+        <PieChart data={data} {...arg} />
         <StyledChartInner>
           <p>{`${status.completed} / ${status.total}`}</p>
           <p>Lessons</p>
