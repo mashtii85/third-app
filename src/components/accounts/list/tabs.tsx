5a76c22ff281c096f9f5a32952b40e38e0477d72
Kristian Heruc <kristian@heruc.com>
2021-09-15 23:52:58 +0200

    chore: tabs linting

--- src/components/accounts/list/tabs.tsx
+++ src/components/accounts/list/tabs.tsx
@@ -10,9 +10,11 @@ import { useRouter } from 'next/router'
 
 // UI
 import { Tabs } from '@drykiss/industry-ui'
+import { Tab } from '../../common/tab'
 import { UserAccounts } from '../list/table/userAccounts'
 import AccountDetails from './view/accountDetails'
 import { ACCOUNT_TABS } from '../../../constants/tabs'
+
 const AccountTabs = () => {
   const { query } = useRouter()
 
@@ -24,12 +26,12 @@ const AccountTabs = () => {
 
   return (
     <Tabs key={tab}>
-      <div active={tab === ACCOUNT_TABS.ACCOUNT} label="Account" activeTab="account">
+      <Tab active={tab === ACCOUNT_TABS.ACCOUNT} label="Account">
         <AccountDetails accountId={accountId} title="Account Details" />
-      </div>
-      <div active={tab === ACCOUNT_TABS.USERS} label="Users" activeTab="users">
+      </Tab>
+      <Tab active={tab === ACCOUNT_TABS.USERS} label="Users">
         <UserAccounts type="accounts" title="Users" accountId={accountId} />
-      </div>
+      </Tab>
     </Tabs>
   )
 }
