0299584fe42814af2fb4dc409af2f6b128a8ecb9
Kristian Heruc <kristian@heruc.com>
2021-09-28 18:51:41 +0200

    feat: refactor api handler and error utils

--- src/utils/api/errors.ts
+++ src/utils/api/errors.ts
@@ -3,9 +3,13 @@
  */
 
 // Next
-import type { NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export const errorHandler = (err: any, res: NextApiResponse): void => {
+export const handleNoMatch = (req: NextApiRequest, res: NextApiResponse): void => {
+  return res.status(405).json({ error: `Method ${req.method} Not Allowed` })
+}
+
+export const handleErrors = (err: any, _req: NextApiRequest, res: NextApiResponse): void => {
   console.error('API Error: ' + err.message)
 
   return res.status(500).json({ error: err.message })
