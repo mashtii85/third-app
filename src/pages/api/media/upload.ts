0aab98ea905b6efbc525d85f6e8923623a56540e
Kristian Heruc <kristian@heruc.com>
2021-09-28 18:52:40 +0200

    feat: implement media upload endpoint

--- src/pages/api/media/upload.ts
+++ src/pages/api/media/upload.ts
@@ -7,45 +7,62 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 // Libs
+import nc from 'next-connect'
 import mimetype from 'mimetype'
+import multer from 'multer'
 
 // Services
-import { UploadParams, uploadS3 } from '../../../services/aws/s3'
+import { uploadS3 } from '../../../services/aws/s3'
 
 // Utils
 import { fileName } from '../../../utils/fileName'
+import { handlerOptions } from '../../../utils/api/handlers'
+import { TE } from '../../../utils/api/errors'
 
 // Types
-interface UploadRequest extends NextApiRequest {
-  file: any
-}
+import { ApiUploadRequest } from '../../../utils/api/types'
+import { UploadParams } from '../../../services/aws/s3/types'
 
-export default async function handler(req: UploadRequest, res: NextApiResponse) {
+const upload = multer({
+  storage: multer.memoryStorage()
+})
+
+const uploadMiddleware = upload.single('file')
+
+const handler = nc<NextApiRequest, NextApiResponse>(handlerOptions)
+
+handler.use(uploadMiddleware)
+
+handler.post(async (req: ApiUploadRequest, res: NextApiResponse) => {
   const {
     body: { bucket, folder },
     file: { buffer, originalname }
   } = req
 
-  try {
-    const name = fileName(originalname)
-    const mimeType = mimetype.lookup(originalname)
+  const name = fileName(originalname)
+  const mimeType = mimetype.lookup(originalname)
 
-    const uploadParams: UploadParams = {
-      fileName: name,
-      mimeType,
-      bucket,
-      folder,
-      buffer
-    }
-
-    const data: any = await uploadS3(uploadParams)
-    if (data) {
-      res.status(200).json({ key: data?.key })
-    }
-  } catch (err) {
-    console.error(err)
-    res.status(400).json({ error: 'Bad Request' })
+  const uploadParams: UploadParams = {
+    fileName: name,
+    mimeType,
+    bucket,
+    folder,
+    buffer
   }
 
-  res.status(200).json({ status: 'success' })
+  const data: any = await uploadS3(uploadParams)
+
+  if (data) {
+    return res.status(200).json({ key: data?.key })
+  } else {
+    TE('Error uploading file')
+  }
+})
+
+export default handler
+
+export const config = {
+  api: {
+    bodyParser: false // Disallow body parsing, consume as stream
+  }
 }
