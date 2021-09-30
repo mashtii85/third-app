fe6430fefb23b8bc93d805b654be7e45acb25f0f
Kristian Heruc <kristian@heruc.com>
2021-09-30 10:07:01 +0200

    feat: improve auth

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -13,30 +13,36 @@ import nc from 'next-connect'
 
 // Utils
 import { handlerOptions } from '../../../utils/api/handlers'
-// import { TE } from '../../../utils/api/errors'
 
-// Mocks
-import { Users } from '../../../mocks/users'
+// GQL
+import { query } from '../../../services/graphqlClient'
+import { GET_USER_BY_EMAIL } from '../../../components/users/queries'
 
 const handler = nc<NextApiRequest, NextApiResponse>(handlerOptions)
 
-handler.post((req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const user = Users.find((u) => u.email === req.body.email)
-
-    if (!user || req.body.password !== 'Lms1234!') {
-      return res.status(401).json({ error: 'Email or password not correct' })
-    }
-
-    const token = jwt.sign({ user }, process.env.JWT_PRIVATE_KEY, {
-      algorithm: 'RS512',
-      expiresIn: '7d'
-    })
-
-    res.status(200).json({ token, user })
-  } else {
-    res.status(400).json({ error: 'Bad Request' })
+handler.post(async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.body.email) {
+    res.status(400).json({ error: 'Email missing' })
   }
+
+  let user = null
+
+  const data = await query(GET_USER_BY_EMAIL, { email: req.body.email })
+
+  if (data?.user) {
+    user = data.user[0]
+  }
+
+  if (!user || req.body.password !== 'Lms1234!') {
+    return res.status(401).json({ error: 'Email or password not correct' })
+  }
+
+  const token = jwt.sign({ user }, process.env.JWT_PRIVATE_KEY, {
+    algorithm: 'RS512',
+    expiresIn: '7d'
+  })
+
+  res.status(200).json({ token, user })
 })
 
 export default handler
