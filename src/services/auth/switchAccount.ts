cdb8a4813b5f14742bf67ea754485342affa7a58
Kristian Heruc <kristian@heruc.com>
2021-10-07 17:26:35 +0200

    feat: prepare for account switcher

---
+++ src/services/auth/switchAccount.ts
@@ -0,0 +1,27 @@
+/**
+ * Services - Auth - Switch Account
+ */
+
+// Utils
+import { TE } from '../../utils/api/errors'
+
+// Types
+import { LoginModel } from './types'
+
+export const switchAccount = async (accountId: number, headers: any): Promise<LoginModel> => {
+  if (!accountId || !headers) {
+    TE('Account ID or token is missing', 401)
+  }
+
+  const userData = {
+    user: {}
+  }
+
+  const token = 'test' // ToDo: generate new token
+
+  if (!token) {
+    TE('Error generating token')
+  }
+
+  return { token, user: userData.user }
+}
