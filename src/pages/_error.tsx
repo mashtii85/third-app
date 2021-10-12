c113fa89781ca760451bb9b6e47d212e35954998
Kristian Heruc <kristian@heruc.com>
2021-10-12 17:35:25 +0200

    fix: add missing prop to ErrorPage

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -8,7 +8,7 @@ import type { NextPage } from 'next'
 // UI
 import { Page, Text } from '@drykiss/industry-ui'
 
-const ErrorPage: NextPage = () => {
+const ErrorPage: NextPage = ({ statusCode }) => {
   return (
     <Page pageHeading={{ heading: '500 ERROR PAGE – NOT FOUND' }}>
       <Text>An Error occurred</Text>
