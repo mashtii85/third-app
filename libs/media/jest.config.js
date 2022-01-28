36d17312f5f1eb5b51afea86c0075b11dad949a2
Kristian Heruc <kristian@heruc.com>
2022-01-28 15:15:49 +0100

    feat: replace images lib with media

--- libs/media/jest.config.js
+++ libs/media/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'images',
+  displayName: 'media',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest'
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/images'
+  coverageDirectory: '../../coverage/libs/media'
 }
